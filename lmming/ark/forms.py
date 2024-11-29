9d028d779ed747b6e0fe68498d20ccd9ad4b899e
Raphaela Heil <RaphaelaHeil@users.noreply.github.com>
2024-11-29 15:44:58 +0100

    [ui] added missing placeholder info

--- lmming/ark/forms.py
+++ lmming/ark/forms.py
@@ -9,20 +9,22 @@ class ArkDetails(Form):
 
 class EditArk(Form):
     ark = CharField(max_length=200, required=False, widget=HiddenInput())
-    url = URLField(max_length=200, label="URL", required=True,
+    url = URLField(max_length=200, label="URL [required]", required=True,
                    widget=URLInput(attrs={'class': 'form-control',
                                           'placeholder': 'Full URL that the ARK should point to'}))
-    title = CharField(max_length=200, required=False, label="Title", widget=TextInput(attrs={'class': 'form-control'}))
-    type = CharField(max_length=200, required=False, label="Type", widget=TextInput(attrs={'class': 'form-control'}))
+    title = CharField(max_length=200, required=False, label="Title",
+                      widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'DC Terms Title'}))
+    type = CharField(max_length=200, required=False, label="Type",
+                     widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'DC Terms Type'}))
     commitment = CharField(max_length=200, required=False, label="Commitment",
-                           widget=TextInput(attrs={'class': 'form-control'}))
+                           widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Commitment'}))
     identifier = CharField(max_length=200, required=False, label="Identifier",
-                           widget=TextInput(attrs={'class': 'form-control'}))
+                           widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'DC Terms Identifier'}))
     format = CharField(max_length=200, required=False, label="Format",
-                       widget=TextInput(attrs={'class': 'form-control'}))
+                       widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'DC Terms Format'}))
     relation = CharField(max_length=200, label="Relation", required=False,
-                        widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'URL to Relation '}))
-    source = URLField(max_length=200, label="Source", required=False,
-                      widget=URLInput(attrs={'class': 'form-control', 'placeholder': 'URL to Source'}))
+                         widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'DC Terms Relation'}))
+    source = URLField(max_length=200, label="Source [URL format]", required=False,
+                      widget=URLInput(attrs={'class': 'form-control', 'placeholder': 'DC Terms Source'}))
     metadata = CharField(max_length=200, required=False, label="Metadata",
-                         widget=TextInput(attrs={'class': 'form-control'}))
+                         widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Miscellaneous Metadata'}))
