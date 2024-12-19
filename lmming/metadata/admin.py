4b83fa283e2cb7b3b7ff7778f5a50911bf1ef183
Raphaela Heil <RaphaelaHeil@users.noreply.github.com>
2024-12-19 17:13:00 +0100

    [feat] admin filtering

--- lmming/metadata/admin.py
+++ lmming/metadata/admin.py
@@ -3,13 +3,20 @@ from django.contrib import admin
 from .models import ExtractionTransfer, Job, Report, Page, ProcessingStep, DefaultValueSettings, \
     DefaultNumberSettings, ExternalRecord, ReportTranslation, FacSpecificData
 
+
+class ProcessingStepAdmin(admin.ModelAdmin):
+    list_filter = ["processingStepType"]
+
 admin.site.register(ExtractionTransfer)
 admin.site.register(Job)
 admin.site.register(Report)
 admin.site.register(Page)
-admin.site.register(ProcessingStep)
+admin.site.register(ProcessingStep, ProcessingStepAdmin)
 admin.site.register(DefaultValueSettings)
 admin.site.register(DefaultNumberSettings)
 admin.site.register(ExternalRecord)
 admin.site.register(ReportTranslation)
 admin.site.register(FacSpecificData)
+
+
+
